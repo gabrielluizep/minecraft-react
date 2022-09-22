245926645f9c72f05601c0d21bbe41a7a88a0a62
gabrielluizep <gabrielluizep@hotmail.com>
2022-09-22 20:40:34 -0300

    :bug: fixed type error in zustand useStore

--- src/hooks/useStore.tsx
+++ src/hooks/useStore.tsx
@@ -1,14 +1,30 @@
 import { nanoid } from 'nanoid'
 import create from 'zustand'
 
-export const useStore = create((set) => ({
-  textures: 'dirt',
+type Cube = {
+  key: string
+  pos: [number, number, number]
+  texture: string
+}
+
+type GameState = {
+  texture: string
+  cubes: Cube[]
+  addCube: (x: number, y: number, z: number) => void
+  removeCube: () => void
+  setTexture: () => void
+  saveWorld: () => void
+  resetWorld: () => void
+}
+
+export const useStore = create<GameState>((set) => ({
+  texture: 'dirt',
   cubes: [{ key: nanoid(), pos: [2, 0.5, 2], texture: 'dirt' }],
   addCube: (x, y, z) => {
     set((prev) => ({ cubes: [{ key: nanoid(), pos: [x, y, z], texture: prev.texture }, ...prev.cubes] }))
   },
-  removeCube: () => {},
-  setTexture: () => {},
-  saveWorld: () => {},
-  resetWorld: () => {},
+  removeCube: () => null,
+  setTexture: () => null,
+  saveWorld: () => null,
+  resetWorld: () => null,
 }))
