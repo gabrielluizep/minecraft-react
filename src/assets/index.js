7c91ffc2a8a4b92c62a2780b1d4cfe0d414e66e1
gabrielluizep <gabrielluizep@hotmail.com>
2022-09-21 21:07:44 -0300

    --no-edit

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import dirtImg from './dirt.png'
+import glassImg from './glass.png'
+import grassImg from './grass.png'
+import logImg from './log.png'
+import woodImg from './wood.png'
+
+export { dirtImg, glassImg, grassImg, logImg, woodImg }
