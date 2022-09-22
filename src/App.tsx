9494f49728dd14cbadd67b6e863cc9c85348245f
gabrielluizep <gabrielluizep@hotmail.com>
2022-09-21 23:14:25 -0300

    :sparkles: added mouse control

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import { Physics } from '@react-three/cannon'
 import { Sky } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Ground, Player } from './components'
+import { FPV } from './components/FPV'
 
 function App() {
   return (
-    <Canvas>
-      <Sky sunPosition={[100, 100, 20]} />
+    <>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
 
-      <ambientLight intensity={0.5} />
-      <Physics>
-        <Ground />
-        <Player />
-      </Physics>
-    </Canvas>
+        <ambientLight intensity={0.5} />
+        <FPV />
+        <Physics>
+          <Ground />
+          <Player />
+        </Physics>
+      </Canvas>
+      <div className="cursor absolute centered">+</div>
+    </>
   )
 }
 
