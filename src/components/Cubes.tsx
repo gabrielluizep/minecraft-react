d367dc26d8bc45d00f77cf47684a4d53a8d79ab3
gabrielluizep <gabrielluizep@hotmail.com>
2022-09-23 12:03:24 -0300

    :mute: removed log

--- src/components/Cubes.tsx
+++ src/components/Cubes.tsx
@@ -5,8 +5,6 @@ import { Cube } from './Cube'
 export const Cubes = () => {
   const [cubes] = useStore((state) => [state.cubes])
 
-  console.log(cubes)
-
   return (
     <>
       {cubes.map(({ key, pos, texture }) => (
