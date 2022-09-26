6ecb5bfb14b48320595e714f9893a4495aeb3029
gabrielluizep <gabrielluizep@hotmail.com>
2022-09-26 12:48:48 -0300

    :truck: added game container to his own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { Physics } from '@react-three/cannon'
-import { Sky } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-
-import { Ground, Player, Cubes, FPV, Menu, TextureSelector } from './components'
+import { Game } from './components'
 
 function App() {
   return (
-    <div className="w-[800px] h-[600px] relative">
-      <Canvas>
-        <Sky sunPosition={[100, 100, 20]} />
-
-        <ambientLight intensity={0.5} />
-        <FPV />
-        <Physics>
-          <Ground />
-          <Player />
-          <Cubes />
-        </Physics>
-      </Canvas>
-      <div className="absolute top-1/2 left-1/2 -translate-x-0 -translte-y-1/2 text-white">+</div>
-      <TextureSelector />
-      <Menu />
+    <div className="flex items-center justify-center h-screen">
+      <Game />
     </div>
   )
 }
---
+++ src/components/Game.tsx
@@ -0,0 +1,25 @@
+import { Physics } from '@react-three/cannon'
+import { Sky } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+
+import { Ground, Player, Cubes, FPV, Menu, TextureSelector } from './'
+
+export const Game = () => {
+  return (
+    <div className="w-[800px] h-[600px] relative">
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <FPV />d
+        <Physics>
+          <Ground />
+          <Player />
+          <Cubes />
+        </Physics>
+      </Canvas>
+      <div className="absolute top-1/2 left-1/2 -translate-x-0 -translte-y-1/2 text-white">+</div>
+      <TextureSelector />
+      <Menu />
+    </div>
+  )
+}
