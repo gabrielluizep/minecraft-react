eef35c9d1c4e713e37a057e0a9855be97b4b0723
gabrielluizep <gabrielluizep@hotmail.com>
2024-01-12 13:35:53 -0300

    fix: added type button to buttons

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -10,12 +10,14 @@ export const Menu = () => {
     <div className="absolute top-3 left-3 space-x-3">
       <button
         onClick={saveWorld}
+        type="button"
         className="inline-flex py-1 px-4 justify-center gap-x-2 rounded-lg text-sm leading-6 font-semibold focus-visible:ring-2 focus:outline-none focus-visible:ring-offset-2 focus-visible:ring-gray-500 bg-gray-900 text-white hover:bg-gray-800"
       >
         Save
       </button>
       <button
         onClick={resetWorld}
+        type="button"
         className="inline-flex py-1 px-4 justify-center gap-x-2 rounded-lg text-sm leading-6 font-semibold focus-visible:ring-2 focus:outline-none focus-visible:ring-offset-2 focus-visible:ring-gray-500 bg-gray-900 text-white hover:bg-gray-800"
       >
         Reset
